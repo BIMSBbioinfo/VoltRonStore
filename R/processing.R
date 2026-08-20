8880d9833ae2e446147c2ca58c7a03c6354856a5
Artur-man <artur-man@hotmail.com>
2026-08-20 17:53:23 +0200

    bump to 0.1.2

---
+++ R/processing.R
@@ -0,0 +1,24 @@
+#' @importFrom VoltRon normalizeData
+#' @importFrom VoltRon getPCA
+#' @importClassesFrom S4Arrays Array
+#' @import BPCells
+
+#' @export
+setMethod("normalizeData", 
+          "IterableMatrix", 
+          VoltRon:::.normalizeDataMatrix)
+
+#' @export
+setMethod("normalizeData", 
+          "Array", 
+          VoltRon:::.normalizeDataMatrix)
+
+#' @export
+setMethod("getPCA", 
+          "IterableMatrix", 
+          VoltRon:::.getPCAMatrix)
+
+#' @export
+setMethod("getPCA", 
+          "Array",
+          VoltRon:::.getPCAMatrix)
